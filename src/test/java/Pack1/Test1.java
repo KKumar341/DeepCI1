41ef27ced934a95b4791e23cd49c1fcbea8eb49f
Thottalgere Krishnamurthy <deepthesh.thottalgere.krishnamurthy@hp.com>
2016-05-22 00:17:53 +0530

    2ndCommit

--- src/test/java/Pack1/Test1.java
+++ src/test/java/Pack1/Test1.java
@@ -2,15 +2,29 @@ package Pack1;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 
 
 public class Test1 {
+	
+	WebDriver driver;
+	
+    @BeforeTest
+    public void beforeTest() {  
+    	System.out.println("<<SYSO>> In Before test");
+        driver = new FirefoxDriver();  
+    }       
+    @AfterTest
+    public void afterTest() {
+    	System.out.println("<<SYSO>> In After test");
+        driver.quit();          
+    }       
 
 	@Test
 	public void method1(){
-		WebDriver driver = new FirefoxDriver();
 		driver.get("https://accounts.google.com/ServiceLogin?passive=1209600&continue=https%3A%2F%2Faccounts.google.com%2FManageAccount&followup=https%3A%2F%2Faccounts.google.com%2FManageAccount#identifier");
 
 		driver.findElement(By.id("Email")).sendKeys("TestUserName");
